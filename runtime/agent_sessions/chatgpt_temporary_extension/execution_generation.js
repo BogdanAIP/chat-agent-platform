6878c1c29d43e86b7303b2fe13e605dc5c399359
Богдан Гайдук <FedoTot1327@yandex.ru>
2026-09-03 20:47:00 +0300

    Regenerate Temporary worker execution identity

--- runtime/agent_sessions/chatgpt_temporary_extension/execution_generation.js
+++ runtime/agent_sessions/chatgpt_temporary_extension/execution_generation.js
@@ -1,4 +1,4 @@
 "use strict";
 
 globalThis.CAPChatGPTTemporaryExecutionGeneration =
-  "cbad870f6c0a9e498229d99fdc6212051f36594f765285e5b3778fbcce60b574";
+  "9eb5768d239fa3df98a65cef53ce5e81582057474d5dcfc30d4007b784cb1057";
